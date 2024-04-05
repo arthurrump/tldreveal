b39bdfc369b54cb65b799ac9032a2155475a7ec7
Arthur Rump <hello@arthurrump.com>
2024-04-05 21:40:19 +0200

    Extend demo presentation

--- demo/index.ts
+++ demo/index.ts
@@ -1,10 +1,17 @@
 import Reveal from "reveal.js"
 import { Tldreveal } from "../src/index"
 
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm"
+import "reveal.js/plugin/highlight/zenburn.css"
+
 import "reveal.js/dist/reveal.css"
 import "reveal.js/dist/theme/white.css"
 
 Reveal.initialize({
+    hash: true,
     scrollActivationWidth: null,
-    plugins: [ Tldreveal ]
+    plugins: [ Tldreveal, RevealHighlight ],
+    tldreveal: {
+        isDarkMode: false
+    }
 })
