3e9302d8918af39d9ca61e811607a42bd80b3508
Arthur Rump <hello@arthurrump.com>
2024-04-13 12:11:05 +0200

    Document configuration options

--- package/src/config.ts
+++ package/src/config.ts
@@ -28,6 +28,8 @@ export const defaultStyleProps = {
 }
 
 export interface TldrevealConfig {
+    /// Configure the default drawing styles for tldraw, used when the editor is
+    /// first started.
     defaultStyles: {
         color?: TLDefaultColorStyle
         size?: TLDefaultSizeStyle
@@ -38,12 +40,28 @@ export interface TldrevealConfig {
         verticalAlign?: TLDefaultVerticalAlignStyle
     }
 
+    /// Show a warning when the `disableLayout` option is enabled. Set to
+    /// `false` to silence the warning when you have provided your own style
+    /// overrides. Defaults to `true`. 
     disableLayoutWarning: boolean
 
+    /// Whether the tldraw UI is rendered in dark mode by default. Set to
+    /// `false` if you use a light theme for Reveal.js, defaults to `true`.
     isDarkMode: boolean
+    /// Allow automatic switching between dark and light mode for slides with a
+    /// custom data-background attribute. Defaults to `true`.
     automaticDarkMode: boolean
 
+    /// Use localStorage to store drawing state in the browser. Requires an `id`
+    /// or `data-tlid` attribute on the .reveal or .slides element to determine
+    /// the identity of the slide deck. Defaults to `true`, if that id is found.
     useLocalStorage: boolean
+    /// Load drawing state from a URI. Set to `false` to disable this option,
+    /// set to `"auto"` to automatically determine the URI from the slide URI or
+    /// set to `{ uri: "/path/to/saved.tldrev" }` to supply a custom location.
+    /// The automatic mode replaces `.html` or `.html` in the slide path with
+    /// `.tldrev` if the path ends with one of those extensions. Else it appends
+    /// `/index.tldrev`.
     snapshotUri: false | "auto" | { uri: string }
 }
 
