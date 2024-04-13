92272dda157ce03e09a97ae736c5c2417256cef1
Arthur Rump <hello@arthurrump.com>
2024-04-13 15:03:38 +0200

    Add installation notes in demo

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -3,6 +3,8 @@ import Reveal from "reveal.js"
 import { Tldreveal } from "tldreveal"
 import "tldreveal/dist/esm/index.css"
 
+import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm"
+
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm"
 import "reveal.js/plugin/highlight/zenburn.css"
 
@@ -12,7 +14,7 @@ import "reveal.js/dist/theme/white.css"
 Reveal.initialize({
     hash: true,
     scrollActivationWidth: undefined,
-    plugins: [ Tldreveal, RevealHighlight ],
+    plugins: [ Tldreveal, RevealMarkdown, RevealHighlight ],
     tldreveal: {
         isDarkMode: false,
         snapshotUri: "auto"
